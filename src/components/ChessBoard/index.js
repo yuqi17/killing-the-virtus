a1822d52ca33215442aafafe0d9cb90a0adc2d73
yuqi <yu.qi@mwee.cn>
2020-02-08 22:03:57 +0800

    备忘录

--- src/components/ChessBoard/index.js
+++ src/components/ChessBoard/index.js
@@ -9,6 +9,7 @@ const CELL_SIZE = 60;
 // update state  to render 不能和直接操作dom动画同时存在
 // 点击格子事件保证准确触发
 // 二维数组若要做备忘录模式，需要深拷贝[...arr],slice(0) 都是浅拷贝
+// 事件是先从子元素开始冒泡的,如果子元素停止冒泡,父组件同样的事件不会触发
 
 export default class ChessBoard extends Component {
 
@@ -24,6 +25,7 @@ export default class ChessBoard extends Component {
   ]
 
   selectedChessMan = null
+  memo = []
 
   // calc(arr, row1,col1,row,col){
   //   const newArr = JSON.parse(JSON.stringify(arr))//arr.slice(0)//[...arr];
@@ -34,6 +36,7 @@ export default class ChessBoard extends Component {
 
   
   handleChessBoardCellClick = (e)=>{
+    
     const col = Math.floor(e.clientX / CELL_SIZE)
     const row = Math.floor(e.clientY / CELL_SIZE)
     const chessManView = e.currentTarget.firstChild
@@ -67,14 +70,31 @@ export default class ChessBoard extends Component {
       console.log(this.mapArr)
 
       //change view
+      this.memo.push({
+        target:this.selectedChessMan.view,
+        op:''
+      })
       this.selectedChessMan.view.style.transform += `translate(${(col - col1) * CELL_SIZE}px,${(row - row1) * CELL_SIZE}px)`
+      this.memo.push({
+        target:this.selectedChessMan.view,
+        op:`translate(${(col - col1) * CELL_SIZE}px,${(row - row1) * CELL_SIZE}px)`
+      })
+
       if(this.mapArr[row][col] !== 0){
+        this.memo.push({
+          target:this.selectedChessMan.view,
+          op:'block'
+        })
         chessManView.style.display = 'none'
+        this.memo.push({
+          target:chessManView,
+          op:'none'
+        })
       }
       this.selectedChessMan = null
+      console.log(this.memo)
     }
   }
-
 
   render() {
     return (
