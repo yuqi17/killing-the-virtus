ffb5bcb492cd08613b417f9075641dc0a1032dfe
yuqi <yu.qi@mwee.cn>
2020-02-08 20:45:00 +0800

    坐标

--- src/components/ChessBoard/index.js
+++ src/components/ChessBoard/index.js
@@ -29,43 +29,49 @@ export default class ChessBoard extends Component {
     return newArr
   }
 
-  handleChessManClick = (row, col)=>{
-    console.log(row,col)
+  handleChessBoardClick = (e)=>{
+    const col = Math.floor(e.clientX / CELL_SIZE)
+    const row = Math.floor(e.clientY / CELL_SIZE)
+
     if(!this.selectedChessMan)
     {
-      console.log(this.mapArr)
-      console.log(row, col,this.mapArr[row][col],'=====')
+      if(this.mapArr[row][col] === 0)
+        return
       this.selectedChessMan = {
         type:this.mapArr[row][col], row, col
       };
     } else {
-      console.log(this.mapArr)
+
       const type = this.mapArr[row][col];
       const { type:type1, row:row1, col:col1 } = this.selectedChessMan
       if(row1 === row && col1 === col)
         return console.log('2')
       if(type1 === type)
         return console.log('3',type1, type)
-      
+      console.log(this.mapArr)
       //change
+      console.log(row1, col1, row, col)
       this.mapArr = this.calc(this.mapArr, row1, col1, row, col)
       console.log(this.mapArr)
+
       const dom = ReactDOM.findDOMNode(this.refs[`${row1}-${col1}`])
       dom.style.transform = `translate(${(col - col1) * CELL_SIZE}px,${(row - row1) * CELL_SIZE}px)`
       this.selectedChessMan = null
+
+      // update state  to render 不能和直接操作dom动画同时存在
     }
   }
 
   render() {
     return (
-      <div id="board">
+      <div id="board" onClick={this.handleChessBoardClick}>
       {
         this.mapArr.map((_,col)=><div className='col' key={col}>
         {
-          _.map((_,row) => <div className='cell' onClick={()=>this.handleChessManClick(row,col)} key={`${row}-${col}`}>
+          _.map((_,row) => <div className='cell' key={`${row}-${col}`}>
             <ChessMan ref={`${row}-${col}`}
               roleType={this.mapArr[row][col]} 
-              />
+            />
           </div>)
         }
         </div>)
