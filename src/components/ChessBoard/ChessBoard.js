5036740eb835de7330ca711ed32ce1fd1dabcf5d
yuqi <yu.qi@mwee.cn>
2020-02-29 15:29:26 +0800

    可以链接

--- src/components/ChessBoard/ChessBoard.js
+++ src/components/ChessBoard/ChessBoard.js
@@ -1,5 +1,6 @@
 
 
+import io from "socket.io-client";
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
@@ -28,40 +29,66 @@ export default class ChessBoard extends Component {
   selectedChessMan = null
   memo = []
 
-  calc(arr, row1,col1,row,col){
+
+  componentDidMount() {
+
+    // 下面这个写法链接不上，还必须有path
+    // var socket = io.connect(`http://localhost:3001/game`)
+
+    let socket = require('socket.io-client')('http://localhost:8080/game',{
+      query:{
+        nickname:Date.now()
+      }
+    });
+
+    socket.on('connect', function () {
+      console.log('connect')
+      socket.emit('chat',{message:111})
+    });
+
+    socket.on('msg', function (data) {
+      console.log(data)
+    });
+
+    socket.on('disconnect', function () {
+      console.log('disconnect')
+    });
+  }
+
+  calc(arr, row1, col1, row, col) {
     const newArr = JSON.parse(JSON.stringify(arr))//arr.slice(0)//[...arr];
     newArr[row][col] = newArr[row1][col1]
     newArr[row1][col1] = 0
     return newArr
   }
 
-  checkWin(arr){
+  checkWin(arr) {
     const amtMonster = arr.flat().filter(item => item === 1).length
-    if(amtMonster < 4){
+    if (amtMonster < 4) {
       return 2
     }
     let sum = 0;
     const len = arr.length
-    for(let i = 0;i < len;i++){
-      for(let j = 0;j < len;j++){
-        if(arr[i][j] ===  2){
-          if(i - 1 >= 0 && arr[i - 1][j] !== 0) {
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len; j++) {
+        if (arr[i][j] === 2) {
+          if (i - 1 >= 0 && arr[i - 1][j] !== 0) {
             sum += 1
           }
-          if(i + 1 < len && arr[i + 1][j] !== 0) {
+          if (i + 1 < len && arr[i + 1][j] !== 0) {
             sum += 1
           }
-          if(i - 1 >= 0 && arr[i][j - 1] !== 0) {
+          if (i - 1 >= 0 && arr[i][j - 1] !== 0) {
             sum += 1
           }
-          if(i + 1 < len && arr[i][j + 1] !== 0) {
+          if (i + 1 < len && arr[i][j + 1] !== 0) {
             sum += 1
           }
         }
       }
     }
 
-    if(sum === 8){
+    if (sum === 8) {
       return 1
     }
 
@@ -70,15 +97,15 @@ export default class ChessBoard extends Component {
 
   /////// 网络版本 myRole 应该由服务器决定先后，然后两个客户端分配不同的myRole
   /////// turn = myRole 该我走，走完后修改turn 值为对方
-  myRole = 1 
+  myRole = 1
   turn = 1
 
   ////////// socket 接收模拟
-  receiveData({ type, data }){
-    if(type === 'move'){
+  receiveData({ type, data }) {
+    if (type === 'move') {
       const { row, col } = data
       this.moveChessMan(row, col)
-    } else if(type === 'turn'){
+    } else if (type === 'turn') {
       this.turn = data.turn
     }
   }
@@ -87,122 +114,121 @@ export default class ChessBoard extends Component {
     const col = Math.floor(e.clientX / CELL_SIZE)
     const row = Math.floor(e.clientY / CELL_SIZE)
     const step = this.moveChessMan(row, col)
-    
+
     // send move data
     // 如果step = 2 还要发送轮流的标志
 
   }
 
-  moveChessMan = (row, col)=>{
+  moveChessMan = (row, col) => {
     const chessManView = ReactDOM.findDOMNode(this.refs[`${row}-${col}`]).firstChild//e.currentTarget.firstChild
-    if(!this.selectedChessMan)
-    {
+    if (!this.selectedChessMan) {
       this.selectedChessMan = {
-        view:chessManView,
-        type:this.mapArr[row][col], row, col
+        view: chessManView,
+        type: this.mapArr[row][col], row, col
       };
       return 1// 第一步
     } else {
       const type = this.mapArr[row][col];
-      const { type:type1, row:row1, col:col1 } = this.selectedChessMan
-     
+      const { type: type1, row: row1, col: col1 } = this.selectedChessMan
+
       // 这个可能要去掉
-      if(type1 !== this.turn){
+      if (type1 !== this.turn) {
         this.selectedChessMan = null
         return console.log('not your turn')
       }
-      
-      if(type1 === 0)//先点击空白格子，没有意义
+
+      if (type1 === 0)//先点击空白格子，没有意义
       {
         this.selectedChessMan = null
         return console.log('0')
       }
 
-      if(row1 === row && col1 === col){
+      if (row1 === row && col1 === col) {
         this.selectedChessMan = null
         return console.log('1')
       }
-        
-      if(type1 === type){
+
+      if (type1 === type) {
         this.selectedChessMan = null
         return console.log('2')
       }
 
       const dx = col1 - col
       const dy = row1 - row
-      const distance = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2))
+      const distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
 
-      if(distance > 1 && distance < 2){
+      if (distance > 1 && distance < 2) {
         this.selectedChessMan = null
         return console.log('no skew')
       }
 
-      if(distance > 2){
+      if (distance > 2) {
         this.selectedChessMan = null
         return console.log('too far')
       }
 
-      if(type1 === 1){
-        if(distance === 2){
+      if (type1 === 1) {
+        if (distance === 2) {
           this.selectedChessMan = null
           return console.log('bat no 2 step')
         }
-        if(type === 2){
+        if (type === 2) {
           this.selectedChessMan = null
           return console.log('can not eat')
         }
       }
 
-      if(type1 === 2){
-        
-        if(distance === 2 && type !== 1){
+      if (type1 === 2) {
+
+        if (distance === 2 && type !== 1) {
           this.selectedChessMan = null
           return console.log('no eat')
         }
-        if(distance === 1 && type === 1){
+        if (distance === 1 && type === 1) {
           this.selectedChessMan = null
           return console.log('no one step eat')
         }
       }
-      
-      if(this.memo.length === 0){
+
+      if (this.memo.length === 0) {
         this.memo.push({
-          map:this.mapArr,
-          actions:[
+          map: this.mapArr,
+          actions: [
             {
-              target:this.selectedChessMan.view,
+              target: this.selectedChessMan.view,
               op: 'init'
             }
           ]
         })
       }
-      
+
       //change data
       this.mapArr = this.calc(this.mapArr, row1, col1, row, col)
       //change view
       this.selectedChessMan.view.style.transform += `translate(${(col - col1) * CELL_SIZE}px,${(row - row1) * CELL_SIZE}px)`
-      if(type !== 0){
+      if (type !== 0) {
         chessManView.style.display = 'none'
         this.memo.push({
-          map:this.mapArr,
-          actions:[
+          map: this.mapArr,
+          actions: [
             {
-              target:this.selectedChessMan.view,
+              target: this.selectedChessMan.view,
               op: 'move'
             },
             {
-              target:chessManView,
-              op:'kill'
+              target: chessManView,
+              op: 'kill'
             },
           ]
         })
       }
-      else{
+      else {
         this.memo.push({
-          map:this.mapArr,
-          actions:[
+          map: this.mapArr,
+          actions: [
             {
-              target:this.selectedChessMan.view,
+              target: this.selectedChessMan.view,
               op: 'move'
             }
           ]
@@ -214,39 +240,38 @@ export default class ChessBoard extends Component {
       // send
       // check win
       const role = this.checkWin(this.mapArr)
-      if(role !== 0){
+      if (role !== 0) {
         console.log(role + ' wined')
       }
       return 2
     }
   }
 
-  moveBack(target){
-    const arr = target.style.transform.replace(/, /g,'*').split(' ')
+  moveBack(target) {
+    const arr = target.style.transform.replace(/, /g, '*').split(' ')
     arr.pop()
     target.style.transform = arr.map(t => t.replace('*', ',')).join(' ')
   }
 
-  memoLastStep(){
-      const { actions, map } = this.memo.pop()
+  memoLastStep() {
+    const { actions, map } = this.memo.pop()
 
-      this.mapArr = map
+    this.mapArr = map
 
-      actions.forEach(action =>{
-        const { target, op} = action
-        if(op === 'kill')
-        {
-          target.style.display = 'block'
-        } else {
-          this.moveBack(target)
-        }
-      })
+    actions.forEach(action => {
+      const { target, op } = action
+      if (op === 'kill') {
+        target.style.display = 'block'
+      } else {
+        this.moveBack(target)
+      }
+    })
   }
 
-  startMemo = () =>{
+  startMemo = () => {
     console.log(this.memo.length)
     const timer = setInterval(() => {
-      if(this.memo.length === 0){
+      if (this.memo.length === 0) {
         return window.clearInterval(timer)
       }
       this.memoLastStep()
@@ -256,15 +281,15 @@ export default class ChessBoard extends Component {
   render() {
     return (
       <div id="board">
-      {
-        this.mapArr.map((_,col)=><div className='col' key={col}>
         {
-          _.map((_,row) => <div onClick={this.handleChessBoardCellClick} className='cell' ref={`${row}-${col}`} key={`${row}-${col}`}>
-            <ChessMan roleType={this.mapArr[row][col]}/>
+          this.mapArr.map((_, col) => <div className='col' key={col}>
+            {
+              _.map((_, row) => <div onClick={this.handleChessBoardCellClick} className='cell' ref={`${row}-${col}`} key={`${row}-${col}`}>
+                <ChessMan roleType={this.mapArr[row][col]} />
+              </div>)
+            }
           </div>)
         }
-        </div>)
-      }
       </div>
     )
   }
